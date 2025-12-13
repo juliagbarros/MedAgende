62fdf1061b96eb2cb33feb05cedf7db9dd33d686
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-12-13 12:44:37 -0300

    Novos métodos em UsuarioDAO
    Adicionado método de buscar o usuário pelo CPF e de atualizar seus dados comuns. Outros dados específicos serão atualizados em outra classe.

--- src/main/java/dao/UsuarioDAO.java
+++ src/main/java/dao/UsuarioDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 import javax.swing.JOptionPane;
 
@@ -46,7 +47,55 @@ public class UsuarioDAO {
 			ConnectionFactory.closeConnection(con,stmt);
 		}
 	}
+	public Usuario buscarPorCpf(String cpf) throws Exception {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
 
+        try {
+            String sql = "SELECT * FROM usuarios WHERE CPF = ?";
+            pst = con.prepareStatement(sql);
+            pst.setString(1, cpf);
+
+            rs = pst.executeQuery();
+
+            if (rs.next()) {
+                Usuario u = new Usuario();
+                u.setIdUsuario(rs.getInt("Id_Usuario"));
+                u.setNome(rs.getString("Nome"));
+                u.setEmail(rs.getString("Email"));
+                u.setTelefone(rs.getString("Telefone"));
+                u.setCpf(rs.getString("CPF"));
+                u.setRua(rs.getString("Rua"));
+                u.setBairro(rs.getString("Bairro"));
+                u.setCidade(rs.getString("Cidade"));
+                u.setCep(rs.getString("CEP"));
+                u.setServico(rs.getString("Servíco"));
+                u.setDataNasc(rs.getDate("Data_Nasc"));
+                return u;
+            }
+
+            return null;
+
+        } finally {
+            ConnectionFactory.closeConnection(con, pst, rs);
+        }
+    }
+	
+	public void atualizarUsuario(Connection con, Usuario u) throws Exception {
+
+	    String sql = "UPDATE usuarios SET Nome = ?, Email = ?, Telefone = ?, Rua = ?, Bairro = ?, Cidade = ?, CEP = ? WHERE CPF = ?";
+
+	    PreparedStatement stmt = con.prepareStatement(sql);
+	    stmt.setString(1, u.getNome());
+	    stmt.setString(2, u.getEmail());
+	    stmt.setString(3, u.getTelefone());
+	    stmt.setString(4, u.getRua());
+	    stmt.setString(5, u.getBairro());
+	    stmt.setString(6, u.getCidade());
+	    stmt.setString(7, u.getCep());
+	    stmt.setString(8, u.getCpf());
+
+	    stmt.executeUpdate();
+	}
 }
-
-
