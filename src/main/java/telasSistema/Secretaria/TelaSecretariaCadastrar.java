2e1d9f5980a1fa525dbdbdd82363accbcae4bae4
Ravenous-project01 <145798793+Ravenous-project01@users.noreply.github.com>
2025-12-13 17:49:46 -0300

    TelaSecretariaCadastrar: placeholder simples pra compilar

---
+++ src/main/java/telasSistema/Secretaria/TelaSecretariaCadastrar.java
@@ -0,0 +1,11 @@
+package telasSistema.Secretaria;
+
+import javax.swing.JFrame;
+
+public class TelaSecretariaCadastrar extends JFrame {
+    public TelaSecretariaCadastrar() {
+        super("Cadastro de Secretaria");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(400,300);
+    }
+}
