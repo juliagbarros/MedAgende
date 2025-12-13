6ab640fd5b4d506f037cccdb835bd9d9f57e5012
Ravenous-project01 <145798793+Ravenous-project01@users.noreply.github.com>
2025-12-13 17:49:45 -0300

    TelaAdminCadastroUsuarios: wrapper simples pra compilar

---
+++ src/main/java/telasSistema/Administrador/TelaAdministradorCadastroUsuarios.java
@@ -0,0 +1,8 @@
+package telasSistema.Administrador;
+
+public class TelaAdministradorCadastroUsuarios extends TelaAdministradorCadastroSecretaria {
+    public TelaAdministradorCadastroUsuarios() {
+        super();
+        // Keep behavior identical to TelaAdministradorCadastroSecretaria for now
+    }
+}
