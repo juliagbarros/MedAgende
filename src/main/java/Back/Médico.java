9f2749c66222a5691066c2c11d76a046b7072823
Ronaldo Cesar <156006721+odlaanoR@users.noreply.github.com>
2025-12-13 12:52:06 -0300

    Classe médico atualizada

--- src/main/java/Back/Médico.java
+++ src/main/java/Back/Médico.java
@@ -1,5 +1,34 @@
 package Back;
 
+import java.sql.Connection;
+
+import dao.MedicoDAO;
+import dao.UsuarioDAO;
+import conexao.ConnectionFactory;
+import model.Usuario;
+
 public class Médico {
 
+    public void editarMedico(Usuario usuario, String crm, String rqe, int especialidade) throws Exception {
+
+        Connection con = ConnectionFactory.getConnection();
+
+        try {
+            con.setAutoCommit(false);
+
+            UsuarioDAO usuarioDAO = new UsuarioDAO();
+            usuarioDAO.atualizarUsuario(con, usuario);
+
+            MedicoDAO medicoDAO = new MedicoDAO();
+            medicoDAO.atualizarMedico(con, usuario.getIdUsuario(),crm, rqe,especialidade);
+
+            con.commit();
+
+        } catch (Exception e) {
+            con.rollback();
+            throw e;
+        } finally {
+            con.setAutoCommit(true);
+        }
+    }
 }
